df6791bc1a31ae7dc3ab3831e0525244e3fab3e6
Shiro_nekos <felisgosal1996@gmail.com>
2022-04-10 22:01:25 +0700

    final

--- Project SEP/resources/views/aboutus.blade.php
+++ Project SEP/resources/views/aboutus.blade.php
@@ -22,7 +22,7 @@
     {{-- <link rel="stylesheet" href="css/responsive.css"> --}}
     <link rel="stylesheet" href="{{ asset('desain/css/Gchoco/responsive.css') }}">
     <!-- fevicon -->
-    <link rel="icon" href="images/fevicon.png" type="image/gif" />
+    {{-- <link rel="icon" href="images/fevicon.png" type="image/gif" /> --}}
     <!-- Tweaks for older IEs-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css"
         media="screen">
@@ -41,6 +41,7 @@
     </div>
     <!-- end loader -->
     <!-- header -->
+    @include('home.Header')
     <div class="wrapper">
         <!-- Sidebar -->
         @include('home.sidebar')
@@ -53,10 +54,10 @@
                     <div class="tm-paging-links">
                         <nav>
                             <ul>
-                                <li class="tm-paging-item"><a href="#" class="tm-paging-link active">Our Product</a></li>
-                                <li class="tm-paging-item"><a href="#" class="tm-paging-link">Sejarah</a></li>
+                                <li class="tm-paging-item"><a href="#" class="tm-paging-link active" id="item-product">Our Product</a></li>
+                                <li class="tm-paging-item"><a href="#" class="tm-paging-link" id="item-history">Our History</a></li>
 
-                                <li class="tm-paging-item"><a href="#" class="tm-paging-link">izakaya</a></li>
+                                <li class="tm-paging-item"><a href="#" class="tm-paging-link" id="item-place">Our Place</a></li>
                             </ul>
                         </nav>
                     </div>
@@ -131,31 +132,36 @@
     <script src="{{ asset('desain/js/gchoco/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('desain/js/gchoco/jquery-3.0.0.min.js') }}"></script>
     <!-- sidebar -->
-    <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+    {{-- <script src="js/jquery.mCustomScrollbar.concat.min.js"></script> --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js"></script>
     <script src="{{ asset('desain/js/gchoco/custom.js') }}"></script>
     <script type="text/javascript">
         baguetteBox.run('.tz-gallery');
     </script>
-    <script>
-        $(document).ready(function() {
-            // Handle click on paging links
-            // $('.tm-paging-link').click(function(e) {
-            //     e.preventDefault();
-
-            //     var page = $(this).text().toLowerCase();
-            //     $('.tm-gallery-page').addClass('hidden');
-            //     $('#tm-gallery-page-' + page).removeClass('hidden');
-            //     $('.tm-paging-link').removeClass('active');
-            //     $(this).addClass("active");
-            // });
-
-        });
-    </script>
 
 
     <script type="text/javascript">
         $(document).ready(function() {
+            $('.tm-paging-link').click(function(e){
+                e.preventDefault();
+                $('.tm-paging-link').removeClass('active');
+                $(this).addClass("active");
+            });
+
+            $('#item-product').on('click',function(){
+                $('.textvideo-judul').empty().append('<h1>OUR PRODUCT</h1>');
+                $('.textvideo-isi').empty().append(' <p> Menyediakan ramen yang mampu memikat banyak orang dari seluruh dunia. Disini kami menyediakan ramen yang  rasanya tidak jauh dari negara sakura. Anda tidak perlu repot lagi sampai harus mengunjungi Jepang untuk menyicipi semangkuk ramen. Kandungan ramen segar yang langsung dibuat di tempat dengan topping sayuran dan daging segar membuat ramen kami menjadi beberapa pilihan terbaik bagi sejumlah orang</p>');
+            });
+
+            $('#item-history').on('click',function(){
+                $('.textvideo-judul').empty().append('<h1>OUR HISTORY</h1>');
+                $('.textvideo-isi').empty().append(' <p> Kami selenium ramen mulai beroperasi sejak 8 Agustus 2022. Tentu saja restoran baru kami bukan menjadi penghalang akan kelezatan menu Ramen di toko kami. Kami memiliki chef yang telah berpengalaman selama 20 tahun di bidangnya dalam membuat ramen. kini outlet kami telah tersedia di Surabaya. Jadi segera kunjungi kami dan rasakan kelezatan dari ramennya ...! </p>');
+            });
+
+            $('#item-place').on('click',function(){
+                $('.textvideo-judul').empty().append('<h1>OUR PLACE</h1>');
+                $('.textvideo-isi').empty().append('<p>Disini kami menawarkan nuansa Jepang, sehingga walaupun kalian tidak ke negara Jepang, kalian tetap dapat merasakan hal yang sama. Selain itu lantai lagu yang dibawa di restoran ini adalah lagu klasik instrumental jepang yang mampu membawa kalian dalam situasi yang nyaman dan rileks , jadi tunggu apalagi segera datang ke sini. </p>');
+            });
 
             $('#sidebarCollapse').on('click', function() {
                 $('#sidebar').toggleClass('active');
