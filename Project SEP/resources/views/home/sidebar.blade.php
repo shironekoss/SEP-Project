73552a5675d73b23e820fea7f3e8d5a999dba55c
Shiro_nekos <felisgosal1996@gmail.com>
2022-04-10 19:29:59 +0700

    tambah sedikit

--- Project SEP/resources/views/home/sidebar.blade.php
+++ Project SEP/resources/views/home/sidebar.blade.php
@@ -29,14 +29,31 @@
             </li>
         @endif
 
-        <li>
-            <a href="{{ URL::to('/selenium') }}/customorder"> <i class="fa fa-angle-right"
-                    aria-hidden="true"></i>Custom
-                Order</a>
+        @if (Request::path() === 'selenium/customorder')
+            <li class="active">
+                <a href="{{ URL::to('/selenium') }}/customorder"> <i class="fa fa-angle-right"
+                        aria-hidden="true"></i>Custom
+                    Order</a>
+            </li>
+        @else
+            <li>
+                <a href="{{ URL::to('/selenium') }}/customorder"> <i class="fa fa-angle-right"
+                        aria-hidden="true"></i>Custom
+                    Order</a>
+            </li>
+        @endif
+
+        @if (Request::path() === 'selenium/aboutus')
+        <li class="active">
+            <a href="{{ URL::to('/selenium') }}/aboutus"> <i class="fa fa-angle-right" aria-hidden="true"></i>Our
+                Restaurant</a>
         </li>
+        @else
         <li>
             <a href="{{ URL::to('/selenium') }}/aboutus"> <i class="fa fa-angle-right" aria-hidden="true"></i>Our
                 Restaurant</a>
         </li>
+        @endif
+
     </ul>
 </nav>
